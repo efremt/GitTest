930cf9ceb91a0e6eb6c316b52a6bc821f2a442e4
Efrem TeferaworkBEYENE <efremt@gmail.com>
2018-09-20 21:26:30 +0700

    My first git check-in (create project)

---
+++ GitRepoTest/GitRepoTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitRepoTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
